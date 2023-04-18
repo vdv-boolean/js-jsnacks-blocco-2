028132a7f32017eca19a775d571ddf11341b6399
vincenzodivita <divitavincenzo@icloud.com>
2023-04-18 14:54:22 +0200

    Add step 1 and 2 of the script

---
+++ jsnack1/assets/script.js
@@ -0,0 +1,15 @@
+/* Consegna dell'esercizio
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. */
+
+//Add an empty array
+const oddNumberArray = [];
+
+console.log(`Numbers in the array before user input: ${oddNumberArray}`);
+
+//Ask a number to user six times 
+const askTimes = 6;
+for (i = 0; i < askTimes; i++) {
+const userInput = parseInt(prompt('Dammi un numero'));
+}

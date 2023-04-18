4bc4bb51f4db73daaa928921da5012a329950cdd
vincenzodivita <divitavincenzo@icloud.com>
2023-04-18 15:44:39 +0200

    Add first two step of the script

---
+++ jsnack2/assets/script.js
@@ -0,0 +1,15 @@
+/* Consegna
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome. */
+
+// Add an array with names
+const names = ['Vincenzo', 'Gianluca', 'Andrea', 'Irene', 'Davide', 'Luca'];
+
+console.log (`Array of names: ${names}.`)
+
+// Add an array with lastnames
+const lastNames = ['Di Vita', 'Minardi', 'Lorenzi', 'Caiazzo', 'Farci', 'Casamassima'];
+
+console.log (`Array of last names: ${lastNames}.`)
+
+// Get a list of random complete names
+
